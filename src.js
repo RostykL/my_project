2ea8de570a7dcbcb6b6e1cd510ad81a467c067c6
RostykL <rostyk.lukavyi842@gmail.com>
2017-05-07 19:37:56 +0300

    Update src.js

--- src.js
+++ src.js
@@ -1,22 +1,24 @@
   $(document).ready(function() {
+    $('i').hide();
     var balls = $('.ball');
      var ball = 0,
          goal = 10, 
          teamA = 0,
          teamB = 0;
     $('.btn').click(function () {
+        $('i').show();
         ball = Math.floor(Math.random() * 20) + 1;  
         if(ball > goal) {
                  balls.css({'margin-left':'620px'});
-                 $('#teamNum2').html(teamA);
                  teamA++
+                 $('#teamNum2').html(teamA);
                  console.log('teamA won  score ' + teamA);
         }else {
                  balls.css({'margin-left':'200px'});
-                 $('#teamNum1').html(teamB);
                  teamB++
+                 $('#teamNum1').html(teamB);
                  console.log('teamB won  score ' + teamB );
-        }      
+        }
     }); 
   });
  
